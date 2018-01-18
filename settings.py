a703a2e520dd1cf9efd78ac4b3e99f84fa226a98
Wenli Du <wenli.du@udc.edu>
2018-01-18 11:58:56 -0500

    game settings

---
+++ settings.py
@@ -0,0 +1,16 @@
+class Settings():
+	def __init__(self):
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (230,230,230)
+		self.speed = 2
+		self.bulletspeed = 3
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = 60,60,60
+		self.bullets_allowed = 5
+		self.alien_speed_factor = 2
+		self.fleet_drop_speed = 11
+		self.fleet_direction = 1
+		self.number_fleets = 3
+		self.number_created = 0
