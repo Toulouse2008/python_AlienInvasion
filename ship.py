823baf03eb4b5bcbc0738f4aa87e31a05316bb27
Wenli Du <wenli.du@udc.edu>
2018-01-19 13:12:08 -0500

    ship updated

--- ship.py
+++ ship.py
@@ -1,29 +1,37 @@
 import pygame
+
 class Ship():
-		def __init__(self,ai_settings,screen):
-			#Iniialize the ship and sey its starting position 
-			self.screen = screen
-			# Load the ship image and get its rect
-			self.image = pygame.image.load("./images/ship.bmp")
-			self.rect = self.image.get_rect()
-			self.screen_rect = screen.get_rect()
-			# Start each new ship at the bottom center of the screen
-			self.rect.centerx = self.screen_rect.centerx
-			self.rect.bottom = self.screen_rect.bottom
-			self.moving_right = False
-			self.moving_left =False
-			self.ai_settings = ai_settings 
+    def __init__(self, screen_settings, screen):
+        '''Initialize the ship and set the starting position'''
+        self.screen = screen
+        self.screen_settings = screen_settings
+        # load the ship image and set its rectangle location
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
 
+        # store ship center
+        self.center = float(self.rect.centerx)
 
-		def update(self):
-			if self.moving_right and self.rect.right<self.screen_rect.right:
-				#setting left and right to true if we press down on left and right 
-				self.rect.centerx+=self.ai_settings.speed
-			elif self.moving_left and self.rect.left>0:
-				self.rect.centerx-=self.ai_settings.speed
+        # movement flags
+        self.moving_right = False
+        self.moving_left = False
 
+        # new ship appears from the bottom center
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
 
+    def blitme(self):
+        '''Draw the ship at its current location'''
+        self.screen.blit(self.image, self.rect)
 
-		def blitme(self):
-			#Draw the ship at its current location
-			self.screen.blit(self.image,self.rect)
+    def center_ship(self):
+        self.center = self.screen_rect.centerx 
+
+    def update(self):
+        '''Update position based on movement flag'''
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.screen_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.screen_settings.ship_speed_factor
+        self.rect.centerx = self.center
