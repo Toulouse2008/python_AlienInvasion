a1a4319cc55c199acbe8402f5ea47371b1d37c0e
Wenli Du <wenli.du@udc.edu>
2018-01-19 15:48:35 -0500

    same stats

--- game_stats.py
+++ game_stats.py
@@ -2,7 +2,12 @@ class GameStats():
     def __init__(self, screen_settings):
         self.screen_settings = screen_settings
         self.reset_stats()
-        self.game_active = True
+        # start from inactive state
+        self.game_active = False
+        # high score should never be reset
+        self.high_score = 0
 
     def reset_stats(self):
         self.ship_left = self.screen_settings.ship_limit
+        self.score = 0
+        self.level = 1
