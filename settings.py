7f14f226b852d8e2fc4159f6fa5da000502283e7
Wenli Du <wenli.du@udc.edu>
2018-01-19 13:11:37 -0500

    update settings

--- settings.py
+++ settings.py
@@ -1,16 +1,23 @@
 class Settings():
-	def __init__(self):
-		self.screen_width = 1200
-		self.screen_height = 800
-		self.bg_color = (230,230,230)
-		self.speed = 2
-		self.bulletspeed = 3
-		self.bullet_width = 3
-		self.bullet_height = 15
-		self.bullet_color = 60,60,60
-		self.bullets_allowed = 5
-		self.alien_speed_factor = 2
-		self.fleet_drop_speed = 11
-		self.fleet_direction = 1
-		self.number_fleets = 3
-		self.number_created = 0
+    ''' Class to store setting parameters'''
+    def __init__(self):
+        '''Initialize the game settings'''
+        # screen parameters: width, height, and color
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        # ship speed factor
+        self.ship_speed_factor = 1.5
+        self.ship_limit = 3
+
+        # bullet settings
+        self.bullets_allowed = 3
+        self.bullet_speed_factor = 3
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color =60, 60, 60
+
+        # alien settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
